564547dd265d060076c26c518f6c79ac360856da
reshangayantha9 <reshangayantha9@gmail.com>
2023-01-30 15:49:22 +0530

    first commit

---
+++ src/grocerysystem/GrocerySystem.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package grocerysystem;
+
+/**
+ *
+ * @author Reshan
+ */
+public class GrocerySystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new login().setVisible(true);
+    }
+    
+}
